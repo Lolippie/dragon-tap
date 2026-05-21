a7bce616d9866f3bd8c08a989716b834cb564199
Nathaniel Vaur Henel <nathaniel@digicrafters.fr>
2026-05-22 00:35:10 +0200

    corrige le port dans board-react

--- board-react/vite.config.ts
+++ board-react/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4184,
+    port: 4182,
   },
   test: {
     environment: 'jsdom',
